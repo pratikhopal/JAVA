a702212b8852960091745f299521bf6ebacbac93
Pratik Hopal <hopalsnap@gmail.com>
2025-01-13 22:22:14 +0530

    my 3rd commit

--- Composition/src/Car.java
+++ Composition/src/Car.java
@@ -2,18 +2,26 @@ public class Car {
     Engine e1;
     Transmission t1;
     tyre ty1;
+    doors d1;
     Car(){
         e1=new Engine();
         t1=new Transmission();
         ty1 = new tyre();
+        d1 = new doors();
     }
     public void startcar(){
+        d1.doors_closed();
         e1.engine();
         t1.transmission();
         ty1.wheelspin();
         System.out.println("The car has started");
     }
 
+}
+class doors{
+    public void doors_closed(){
+        System.out.println("the doors are closed");
+    }
 }
 class Engine{
     public void engine(){
